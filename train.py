40f63b8f00d947e4632ca6a5aa742646d4c702a3
kingstdio <kingstdio@gmail.com>
2021-07-05 17:02:02 +0800

    add train.py

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 from jax_unirep import get_reps
 from xgboost import XGBClassifier
 import os
+import subprocess
 
 
 table_head = [  'id', 
@@ -39,7 +40,7 @@ def load_ec_lable_dict(filepath):
 #endregion
 
 #region 按照分割日期加载训练数据
-def load_train_file(split_date, cnx_ecnumber, type='enzyme'):
+def load_train_file(split_date, cnx_ecnumber, type):
     """[按照分割日期加载训练数据]
 
     Args:
@@ -101,7 +102,7 @@ def get_enzyme_train_set(train_date, eclablefile, featurefile, cnx, trainX='enzy
     else:
         #加载训练数据
         dict_ec_label, dict_label_ec = load_ec_lable_dict(eclablefile)
-        train_data = load_train_file(split_date= train_date, cnx_ecnumber=cnx, type='ec')
+        train_data = load_train_file(split_date= train_date, cnx_ecnumber=cnx, type='enzyme')
         
         #添加训练标签
         train_data_labeled  = add_ec_label(train_data, dict_ec_label)
@@ -129,14 +130,27 @@ def xgmain(X_train_std, Y_train, model_file):
 def train_isenzyme(X,Y, model_file):
     xgmain(X,Y,model_file)
 
-
+#region 获取EC号训练的数据集
 def get_ec_train_set(train_date, eclabelfile, featurefile, cnx, trainX='ec_train_x.feather', trainY='ec_train_y.feather'):
+    """获取EC号训练的数据集
+    Args:
+        train_date ([string]]): [划分训练测试的时间]
+        eclabelfile ([string]): [标签文件]
+        featurefile ([string]): [特征文件]
+        cnx ([string]): [数据库连接字符串]
+        trainX (str, optional): [保存的x文件]. Defaults to 'ec_train_x.feather'.
+        trainY (str, optional): [保存的y文件]. Defaults to 'ec_train_y.feather'.
+
+    Returns:
+        [DataFrame]: [trian_X, train_Y]
+    """
     if os.path.exists(trainX):
         trian_X = pd.read_feather(trainX)
         train_Y = pd.read_feather(trainY)
     else:
         dict_ec_label, dict_label_ec = load_ec_lable_dict(eclabelfile)
-        train_data = load_train_file(split_date= train_date, cnx_ecnumber=cnx)
+        
+        train_data = load_train_file(split_date= train_date, cnx_ecnumber=cnx, type='ec')
         train_data_labeled  = add_ec_label(train_data, dict_ec_label)
         unirep_data = load_unirep(featurefile)
         res_data = train_data_labeled.merge(unirep_data, on='id', how='left')
@@ -144,11 +158,55 @@ def get_ec_train_set(train_date, eclabelfile, featurefile, cnx, trainX='ec_train
         trian_X = res_data.iloc[:,9:1909]
         trian_X.to_feather(trainX)
         train_Y.to_feather(trainY)
-
     return trian_X, train_Y
+#endregion
+
+#region 创建slice需要的数据文件
+def to_file_matrix(file, ds, col_num, stype='label'):
+    """[创建slice需要的数据文件]
+
+    Args:
+        file ([string]): [要保存的文件名]
+        ds ([DataFrame]): [数据]
+        col_num ([int]): [有多少列]
+        stype (str, optional): [文件类型：feature，label]. Defaults to 'label'.
+    """
+    if os.path.exists(file):
+        return 'file exist'
+    
+    if stype== 'label':
+        seps = ':'
+    if stype == 'feature':
+        seps = ' '
+    ds.to_csv(file, index= 0, header =0 , sep= seps)
+
+    cmd ='''sed -i '1i {0} {1}' {2}'''.format(len(ds), col_num, file)
+    os.system(cmd)
+#endregion
+
+def prepare_slice_file(x_data, y_data, x_file, y_file):
+    if ~os.path.exists(x_file):
+         to_file_matrix(file=x_file, ds=x_data.round(4), col_num=1900, stype='feature')
+    if ~os.path.exists(y_file):
+        # 标签再整理，缩减没有出现的标签
+        # ec_lb2lb_dict = {v: k for v,k in zip( set(y_data), range(len(set(y_data))))}
+        # np.save('./data/ec_lb2lb_dict.npy', ec_lb2lb_dict) 
+        # y_save_data = y_data.apply(lambda x: ec_lb2lb_dict.get(x))
+        # to_file_matrix(file=y_file,ds=y_save_data, col_num=len(set(y_data)), stype='label')
+        
+        to_file_matrix(file=y_file,ds=y_data, col_num=len(set(y_data)), stype='label')
+    print('Write success')
+
+
+def train_ec_slice(trainX, trainY, modelPath):
+
+   cmd = ''' ./slice_train {0} {1} {2} -m 100 -c 300 -s 300 -k 700 -o 32 -t 32 -C 1 -f 0.000001 -siter 20 -stype 0 -q 0 '''.format(trainX, trainY, modelPath)
+   print(cmd)
+   os.system(cmd)
+   print('train finished')
 
 if __name__ =="__main__":
-    #1. 数据库连接
+    # 1. 数据库连接
     uctools =  ucTools('172.16.25.20')
     cnx_ecnumber = uctools.db_conn()
 
@@ -169,8 +227,8 @@ if __name__ =="__main__":
    
 
     #3. 「酶｜非酶」模型训练
-    # file_isenzyme_model = './model/isenzyme.model'
-    # train_isenzyme(enzyme_X, enzyme_Y.enzyme_label, model_file=file_isenzyme_model)
+    file_isenzyme_model = './model/isenzyme.model'
+    train_isenzyme(enzyme_X.iloc[0:200,:], enzyme_Y.enzyme_label.iloc[0:200], model_file=file_isenzyme_model)
 
     #4. 加载EC号训练数据
     file_ec_train_x = './data/preprocess/ec_train_x.feather'
@@ -182,10 +240,16 @@ if __name__ =="__main__":
                                     trainX=file_ec_train_x,
                                     trainY=file_ec_train_y
                                 )
-
-
-
+    #5. 构建Slice文件
+    file_slice_train_x ='./data/preprocess/ec_slice_train_x.txt'
+    file_slice_train_y ='./data/preprocess/ec_slice_train_y.txt'
+    ec_Y['tags'] = 1
+    prepare_slice_file(x_data=ec_X,y_data=ec_Y[['ec_label', 'tags']],x_file=file_slice_train_x, y_file=file_slice_train_y)
+    
+    file_ec_slice_model = './model'
+    train_ec_slice(trainX=file_slice_train_x, trainY=file_slice_train_y, modelPath=file_ec_slice_model)
     # print(train_data_labeled)
+
 
     
     print('ok')
